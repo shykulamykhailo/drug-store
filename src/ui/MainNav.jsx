761fc3d7ce84dc003936c4c70f26569c238b6e39
shykulamykhailo <lucamike332@gmail.com>
2024-12-22 17:42:12 +0200

    styles: add hamnurger menu and flexible styles for MainNav

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,10 +1,55 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { useUser } from '../features/authentication/useUser';
+import { useEffect, useRef, useState } from 'react';
 
 const StyledNav = styled.nav`
+    display: none;
+
+    @media (min-width: 768px) {
+        display: flex;
+        flex-direction: row;
+        gap: 1.5rem;
+    }
+`;
+
+const HamburgerMenu = styled.div`
     display: flex;
-    gap: 10px;
+    flex-direction: column;
+    gap: 5px;
+
+    div {
+        width: 25px;
+        height: 5px;
+        background-color: black;
+        border-radius: 2px;
+    }
+
+    @media (min-width: 768px) {
+        display: none;
+    }
+`;
+
+const MobileMenu = styled.div`
+    display: ${(props) => (props['data-is-open'] ? 'block' : 'none')};
+    position: absolute;
+    top: 3rem;
+    right: 1rem;
+    background: white;
+    padding: 15px;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    z-index: 99;
+
+    nav {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    @media (min-width: 768px) {
+        display: none;
+    }
 `;
 
 const StyledNavLink = styled(NavLink)`
@@ -25,43 +70,119 @@ const StyledNavLink = styled(NavLink)`
 
 function MainNav() {
     const { isAuthenticated } = useUser();
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    const toggleMenu = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+        if (isOpen) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.addEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.addEventListener('mousedown', handleClickOutside);
+        };
+    }, [isOpen]);
 
     return (
-        <StyledNav>
-            <StyledNavLink
-                to="store"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-                Store
-            </StyledNavLink>
-            <StyledNavLink
-                to="shoppingCart"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-                Shopping cart
-            </StyledNavLink>
-            <StyledNavLink
-                to="contacts"
-                className={({ isActive }) => (isActive ? 'active' : '')}
-            >
-                Contacts
-            </StyledNavLink>
-            {isAuthenticated ? (
+        <>
+            <HamburgerMenu onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </HamburgerMenu>
+            <MobileMenu ref={menuRef} data-is-open={isOpen}>
+                <StyledNav>
+                    <StyledNavLink
+                        to="store"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Store
+                    </StyledNavLink>
+                    <StyledNavLink
+                        to="shoppingCart"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Shopping cart
+                    </StyledNavLink>
+                    <StyledNavLink
+                        to="contacts"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Contacts
+                    </StyledNavLink>
+                    {isAuthenticated ? (
+                        <StyledNavLink
+                            to="profile"
+                            className={({ isActive }) =>
+                                isActive ? 'active' : ''
+                            }
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Profile
+                        </StyledNavLink>
+                    ) : (
+                        <StyledNavLink
+                            to="authorization"
+                            className={({ isActive }) =>
+                                isActive ? 'active' : ''
+                            }
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Authorization
+                        </StyledNavLink>
+                    )}
+                </StyledNav>
+            </MobileMenu>
+            <StyledNav>
                 <StyledNavLink
-                    to="profile"
+                    to="store"
                     className={({ isActive }) => (isActive ? 'active' : '')}
                 >
-                    Profile
+                    Store
                 </StyledNavLink>
-            ) : (
                 <StyledNavLink
-                    to="authorization"
+                    to="shoppingCart"
                     className={({ isActive }) => (isActive ? 'active' : '')}
                 >
-                    Authorization
+                    Shopping cart
                 </StyledNavLink>
-            )}
-        </StyledNav>
+                <StyledNavLink
+                    to="contacts"
+                    className={({ isActive }) => (isActive ? 'active' : '')}
+                >
+                    Contacts
+                </StyledNavLink>
+                {isAuthenticated ? (
+                    <StyledNavLink
+                        to="profile"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                    >
+                        Profile
+                    </StyledNavLink>
+                ) : (
+                    <StyledNavLink
+                        to="authorization"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                    >
+                        Authorization
+                    </StyledNavLink>
+                )}
+            </StyledNav>
+        </>
     );
 }
 
